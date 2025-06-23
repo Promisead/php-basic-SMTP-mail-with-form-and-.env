7480bf71c07b99cab9b3486715d5d0a68c13c9b2
Promise Duke <promiseduke@gmail.com>
2025-06-24 00:32:59 +0100

    add env vars added

--- send-mail.php
+++ send-mail.php
@@ -1,9 +1,14 @@
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
+
 require "vendor/autoload.php";
 
-if ($_SERVER['REQUEST_METHOD']=="POST") {
+// Load environment variables
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $name = $_POST['name'];
     $email = $_POST['email'];
     $message = $_POST['message'];
@@ -11,30 +16,30 @@ if ($_SERVER['REQUEST_METHOD']=="POST") {
     $mail = new PHPMailer(true);
 
     try {
-        $mail -> isSMTP();
-        $mail -> Host = "smtp.gmail.com";
-        $mail -> SMTPAuth = true;
-        $mail -> Username = "thechampionsacademy101@gmail.com";
-        $mail -> Password = "gxic rlmj ebhj tgls";
-        $mail -> SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-        $mail -> Port = 587;
+        $mail->isSMTP();
+        $mail->Host = $_ENV['SMTP_HOST'];
+        $mail->SMTPAuth = true;
+        $mail->Username = $_ENV['SMTP_USERNAME'];
+        $mail->Password = $_ENV['SMTP_PASSWORD'];
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port = $_ENV['SMTP_PORT'];
 
-        $mail -> setFrom("test@test.com", "Aktar");
-        $mail -> addAddress("thechampionsacademy101@gmail.com", "Zaman");
+        $mail->setFrom($_ENV['SMTP_FROM_EMAIL'], $_ENV['SMTP_FROM_NAME']);
+        $mail->addAddress($_ENV['SMTP_TO_EMAIL'], $_ENV['SMTP_TO_NAME']);
 
-        $mail -> Subject = "New Contact Form Submission";
-        $mail -> Body = "Name: $name\n".
-                        "Email: $email\n".
-                        "Message: $message";
-        if($mail -> send()){
+        $mail->Subject = "New Contact Form Submission";
+        $mail->Body = "Name: $name\n" .
+                      "Email: $email\n" .
+                      "Message: $message";
+
+        if ($mail->send()) {
             echo "Message sent successfully";
-        }else{
-            echo "Message could not be sent, Error: " . $mail->ErrorInfo; 
+        } else {
+            echo "Message could not be sent. Error: " . $mail->ErrorInfo;
         }
 
     } catch (Exception $e) {
-        echo "Message could not be sent, Error: " . $mail->ErrorInfo; 
+        echo "Message could not be sent. Error: " . $mail->ErrorInfo;
     }
-    
 }
 ?>
